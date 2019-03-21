03d91b143581ab02b51749d638e6a23ef27db741
Justin Wu <justinhwu95@gmail.com>
2019-03-21 20:08:06 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,9 +2,5 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     array = []
-    animals.each do |a,b|
-      b.has_value?(arguments)
-      return array.push(a)
-    end
-  end
+    
 end
